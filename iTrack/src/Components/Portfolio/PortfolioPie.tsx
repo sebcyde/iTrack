326488f5ed3e4fab7be8174dab2f4df97c7a714e
Seb <sebcyde@hotmail.com>
2022-12-23 21:13:23 +0000

    more progress on portfolio page

---
+++ iTrack/src/Components/Portfolio/PortfolioPie.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const PortfolioPie = (props: Props) => {
+	return <div>PortfolioPie</div>;
+};
+
+export default PortfolioPie;
--- iTrack/src/Pages/Portfolio/Portfolio.tsx
+++ iTrack/src/Pages/Portfolio/Portfolio.tsx
@@ -1,14 +1,18 @@
+import axios from 'axios';
 import { doc, getFirestore } from 'firebase/firestore';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useDocument } from 'react-firebase-hooks/firestore';
 import BottomNavbar from '../../Components/BottomNavbar/BottomNavbar';
+import PortfolioPie from '../../Components/Portfolio/PortfolioPie';
 import SearchNavbar from '../../Components/TopNavbars/SearchNavbar';
 import { app, auth } from '../../Config/firebase';
+import LoadingPage from '../Loading/LoadingPage';
+import Loading from '../Loading/LoadingPage';
 
-type Props = {};
-
-const Portfolio = (props: Props) => {
+const Portfolio = () => {
+	const [Loading, setLoading] = useState(true);
+	const APIKey = 'E8FAQ4X1Q5P2WHPN';
 	const [user, loading, error] = useAuthState(auth);
 	const [UserPortfolio, loading2, error2] = useDocument(
 		doc(getFirestore(app), `Users/${user?.uid}/StockLists/Portfolio`),
@@ -22,10 +26,22 @@ const Portfolio = (props: Props) => {
 		console.log('User Portfolio:', UserPortfolio?.data());
 	};
 
+	const PopulateData = async () => {
+		const Data = await axios.get(
+			`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=${APIKey}`
+		);
+		console.log(Data);
+	};
+
+	useEffect(() => {
+		PopulateData();
+	}, []);
+
 	return (
 		<div>
 			<SearchNavbar />
 			<button onClick={PullData}>Pull Data</button>
+			{Loading ? <LoadingPage /> : <PortfolioPie />}
 			<BottomNavbar />
 		</div>
 	);
